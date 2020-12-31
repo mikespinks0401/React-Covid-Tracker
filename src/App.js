05dbb4aac7542a121d91ac0f61d5bbaf1189f3af
SpinksM <mikespinks0401@gmail.com>
2020-12-31 11:03:50 -0600

    covid-project

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { Cards, Chart, CountryPicker } from './components';
+import { fetchData } from "./api";
+import styles from "./App.module.css";
+
+import covidImg from './images/covid.png'
+
+class App extends React.Component {
+
+state = {
+    data: {},
+    country: "",
+}
+
+async componentDidMount() {
+    const fetchedData = await fetchData();
+    
+    this.setState({ data: fetchedData });
+
+}
+
+
+handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country });
+}
+
+    render() {
+        const { data, country } = this.state;
+        return (
+            <div className={styles.container}>
+                <img className={styles.image} src={covidImg} alt="COVID-19"/>
+                <Cards data={data}/>
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
+                <h1>App</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
